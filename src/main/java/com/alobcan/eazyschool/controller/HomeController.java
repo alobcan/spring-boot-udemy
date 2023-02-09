e0d849622b71e6c198dbc52bb7bc7f9d40e21654
Alejandro Obando <alejandro.obando@unosquare.com>
2023-02-09 10:46:14 -0400

    Building dynamic content using Thymeleaf (Lesson 73)

--- src/main/java/com/alobcan/eazyschool/controller/HomeController.java
+++ src/main/java/com/alobcan/eazyschool/controller/HomeController.java
@@ -1,13 +1,15 @@
 package com.alobcan.eazyschool.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping(value = {"", "/", "/home"})
-    public String displayHomePage() {
+    public String displayHomePage(Model model) {
+        model.addAttribute("username", "Juan Zapata");
         return "home.html";
     }
 }
