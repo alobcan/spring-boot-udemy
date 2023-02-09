f45794644125a8642dd52b5cb2b64335a572f58a
Alejandro Obando <alejandro.obando@unosquare.com>
2023-02-09 17:46:11 -0400

    @RequestScope example

--- src/main/java/com/alobcan/eazyschool/service/ContactService.java
+++ src/main/java/com/alobcan/eazyschool/service/ContactService.java
@@ -1,12 +1,23 @@
 package com.alobcan.eazyschool.service;
 
 import com.alobcan.eazyschool.model.Contact;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.RequestScope;
 
 @Service
 @Slf4j
+@RequestScope
+@Getter
+@Setter
 public class ContactService {
+    private int counter = 0;
+
+    public ContactService() {
+        System.out.println("Contact service Bean initialized");
+    }
 
     public boolean saveMessageDetails(Contact contact) {
         boolean isSaved = true;
