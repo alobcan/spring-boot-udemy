038c4f802bfb4a3a9696cd1da3334e1b1432ace9
Alejandro Obando <alejandro.obando@unosquare.com>
2023-02-09 15:42:04 -0400

    Added Lombok (Lesson 90)

--- src/main/java/com/alobcan/eazyschool/model/Holiday.java
+++ src/main/java/com/alobcan/eazyschool/model/Holiday.java
@@ -1,29 +1,14 @@
 package com.alobcan.eazyschool.model;
 
+import lombok.Data;
+
+@Data
 public class Holiday {
-    private String day;
-    private String reason;
-    private Type type;
+    private final String day;
+    private final String reason;
+    private final Type type;
 
     public enum Type {
         FESTIVAL, FEDERAL
-    }
-
-    public Holiday(String day, String reason, Type type) {
-        this.day = day;
-        this.reason = reason;
-        this.type = type;
-    }
-
-    public String getDay() {
-        return day;
-    }
-
-    public String getReason() {
-        return reason;
-    }
-
-    public Type getType() {
-        return type;
     }
 }
