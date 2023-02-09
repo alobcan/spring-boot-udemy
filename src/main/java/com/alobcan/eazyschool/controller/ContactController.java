92488b791366faabb811a255e88586e6acead62a
Alejandro Obando <alejandro.obando@unosquare.com>
2023-02-09 17:38:10 -0400

    Added some validations

--- src/main/java/com/alobcan/eazyschool/controller/ContactController.java
+++ src/main/java/com/alobcan/eazyschool/controller/ContactController.java
@@ -2,30 +2,37 @@ package com.alobcan.eazyschool.controller;
 
 import com.alobcan.eazyschool.model.Contact;
 import com.alobcan.eazyschool.service.ContactService;
-import org.slf4j.LoggerFactory;
-import org.slf4j.Logger;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
+@Slf4j
 public class ContactController {
 
     @Autowired
     private ContactService contactService;
 
     @RequestMapping("/contact")
-    public String displayContactPage() {
+    public String displayContactPage(Model model) {
+        model.addAttribute("contact", new Contact());
         return "contact.html";
     }
 
     @PostMapping(value = "/saveMsg")
-    public ModelAndView saveMessage(Contact contact) {
+    public String saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors) {
+        if(errors.hasErrors()) {
+            log.error("Contact form validation failed due to: " + errors.toString());
+            return "contact.html";
+        }
         contactService.saveMessageDetails(contact);
-        return new ModelAndView("redirect:/contact");
+        return "redirect:/contact";
     }
 }
