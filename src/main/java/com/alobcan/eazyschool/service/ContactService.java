9f663287fde6e32b5ad0158483cfcdee64826385
Alejandro Obando <alejandro.obando@unosquare.com>
2023-02-09 17:53:36 -0400

    Added @ApplicationScope example

--- src/main/java/com/alobcan/eazyschool/service/ContactService.java
+++ src/main/java/com/alobcan/eazyschool/service/ContactService.java
@@ -5,11 +5,11 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.context.annotation.SessionScope;
+import org.springframework.web.context.annotation.ApplicationScope;
 
 @Service
 @Slf4j
-@SessionScope
+@ApplicationScope
 @Getter
 @Setter
 public class ContactService {
