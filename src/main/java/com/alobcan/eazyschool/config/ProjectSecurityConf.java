fa5c78a79eae0d6611dd1067012cc1b35d1b0fe3
Alejandro Obando <alejandro.obando@unosquare.com>
2023-02-10 11:21:07 -0400

    Added custom security configurations (Lesson 113)

--- src/main/java/com/alobcan/eazyschool/config/ProjectSecurityConf.java
+++ src/main/java/com/alobcan/eazyschool/config/ProjectSecurityConf.java
@@ -9,7 +9,14 @@ import org.springframework.security.web.SecurityFilterChain;
 public class ProjectSecurityConf {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests().anyRequest().denyAll()
+        http.authorizeHttpRequests()
+                .requestMatchers("/home").permitAll()
+                .requestMatchers("/holidays/**").permitAll()
+                .requestMatchers("/contact").permitAll()
+                .requestMatchers("/savemsg").permitAll()
+                .requestMatchers("/courses").authenticated()
+                .requestMatchers("/about").authenticated()
+                .requestMatchers("/assets/**").permitAll()
                 .and().formLogin()
                 .and().httpBasic();
         return http.build();
