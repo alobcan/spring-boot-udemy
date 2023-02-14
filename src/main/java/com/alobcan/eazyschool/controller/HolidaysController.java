a144af67eacb102b14d965659139cc5b37dc8e04
Alejandro Obando <alejandro.obando@unosquare.com>
2023-02-14 09:25:54 -0400

    created_at TIMESTAMP NOT NULL,

--- src/main/java/com/alobcan/eazyschool/controller/HolidaysController.java
+++ src/main/java/com/alobcan/eazyschool/controller/HolidaysController.java
@@ -2,6 +2,8 @@ package com.alobcan.eazyschool.controller;
 
 
 import com.alobcan.eazyschool.model.Holiday;
+import com.alobcan.eazyschool.repository.HolidaysRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,9 @@ import java.util.stream.Collectors;
 
 @Controller
 public class HolidaysController {
+
+    @Autowired
+    private HolidaysRepository holidaysRepository;
 
     @GetMapping("/holidays/{display}")
     public String displayHolidays(@PathVariable String display,
@@ -36,16 +41,7 @@ public class HolidaysController {
             }
         }
 
-        List<Holiday> holidays = Arrays.asList(
-                new Holiday(" Jan 1 ","New Year's Day", Holiday.Type.FESTIVAL),
-                new Holiday(" Oct 31 ","Halloween", Holiday.Type.FESTIVAL),
-                new Holiday(" Nov 24 ","Thanksgiving Day", Holiday.Type.FESTIVAL),
-                new Holiday(" Dec 25 ","Christmas", Holiday.Type.FESTIVAL),
-                new Holiday(" Jan 17 ","Martin Luther King Jr. Day", Holiday.Type.FEDERAL),
-                new Holiday(" July 4 ","Independence Day", Holiday.Type.FEDERAL),
-                new Holiday(" Sep 5 ","Labor Day", Holiday.Type.FEDERAL),
-                new Holiday(" Nov 11 ","Veterans Day", Holiday.Type.FEDERAL)
-        );
+        List<Holiday> holidays = holidaysRepository.findAllHolidays();
         Holiday.Type[] types = Holiday.Type.values();
         for (Holiday.Type type : types) {
             model.addAttribute(type.toString(),
