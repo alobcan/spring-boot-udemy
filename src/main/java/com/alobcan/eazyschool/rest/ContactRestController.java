1c54bb416949594b8ebfba437a7098b3f3fc2f46
Alejandro Obando <alejandro.obando@unosquare.com>
2023-02-17 00:12:10 -0400

    Implement global error logic for Rest Services using @RestControllerAdvice

--- src/main/java/com/alobcan/eazyschool/rest/ContactRestController.java
+++ src/main/java/com/alobcan/eazyschool/rest/ContactRestController.java
@@ -1,14 +1,23 @@
 package com.alobcan.eazyschool.rest;
 
+import com.alobcan.eazyschool.constants.EazySchoolConstants;
 import com.alobcan.eazyschool.model.Contact;
+import com.alobcan.eazyschool.model.Response;
 import com.alobcan.eazyschool.repository.ContactRepository;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -29,6 +38,51 @@ public class ContactRestController {
         } else {
             return List.of();
         }
+    }
+
+    @PostMapping("/saveMsg")
+    public ResponseEntity<Response> saveMsg(@RequestHeader("invocationFrom") String invocationFrom,
+                                            @Valid @RequestBody Contact contact) {
+        log.info(String.format("Header invocationFrom = %s", invocationFrom));
+        contactRepository.save(contact);
+        Response response = new Response();
+        response.setStatusCode("200");
+        response.setStatusMsg("Message saved successfully");
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header("isMsgSaved", "true")
+                .body(response);
+    }
+
+    @DeleteMapping("/deleteMsg")
+    public ResponseEntity<Response> deleteMsg(RequestEntity<Contact> requestEntity) {
+        HttpHeaders headers = requestEntity.getHeaders();
+        headers.forEach((key, value) -> {
+            log.info(String.format("Header '%s' = %s", key, value.stream().collect(Collectors.joining("|"))));
+        });
+        Contact contact = requestEntity.getBody();
+        contactRepository.deleteById(contact.getContactId());
+        Response response = new Response("200", "Message successfully deleted");
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @PatchMapping("/closeMsg")
+    public ResponseEntity<Response> closeMsg(@RequestBody Contact contactReq) {
+        Response response = new Response();
+        Optional<Contact> contact = contactRepository.findById(contactReq.getContactId());
+        if (contact.isPresent()) {
+            contact.get().setStatus(EazySchoolConstants.CLOSE);
+            contactRepository.save(contact.get());
+        } else {
+            response.setStatusCode("400");
+            response.setStatusMsg("Invalid contact ID received");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
+        response.setStatusCode("200");
+        response.setStatusMsg("Message successfully closed");
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(response);
 
     }
 }
